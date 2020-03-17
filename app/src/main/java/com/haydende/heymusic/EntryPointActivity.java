00a90ecd87e269b45c76522979da9d833025ed18
haydende <haydeneastwell@outlook.com>
2020-03-17 17:19:01 +0000

    Added WIP implementation of using MediaStore API
    Close to being able to read the device audio files but creating the cursor necessary for obtaining the data is causing an issue.

--- app/src/main/java/com/haydende/heymusic/EntryPointActivity.java
+++ app/src/main/java/com/haydende/heymusic/EntryPointActivity.java
@@ -1,32 +1,62 @@
 package com.haydende.heymusic;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
  * Activity will be the first to start once application has started.
  * <p>This is where the user will be prompted to accept Read/Write permissions.</p>
  */
-public class EntryPointActivity extends AppCompatActivity {
+public class EntryPointActivity extends AppCompatActivity
+                                implements LoaderManager.LoaderCallbacks<Cursor> {
 
     /**
      * Used to capture user's permission response for READ_EXTERNAL_STORAGE_PERMISSION.
      */
     private final static int READ_EXTERNAL_STORAGE_PERMISSION = 0;
 
+    /**
+     * ID for the MediaStore loader used to gather audio files.
+     */
+    private final static int MEDIASTORE_LOADER_ID = 0;
+
+    /**
+     * Defines the query for what data should be collected using MediaStore.
+     * <p>Used for creating the <code>Cursor</code> mediaStoreCursor.</p>
+     */
+    private String [] projection = {MediaStore.Files.FileColumns._ID,
+                                    MediaStore.Files.FileColumns.DATE_ADDED,
+                                    MediaStore.Files.FileColumns.DURATION};
+
+    /**
+     * This is used to obtain the data from <code>MediaStore</code>.
+     */
+    private Cursor mediaStoreCursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_point);
         checkReadExternalStoragePermission();
+        mediaStoreCursor = getCursor(this);
+        getArtists(0);
     }
 
     /**
@@ -39,7 +69,7 @@ public class EntryPointActivity extends AppCompatActivity {
             // if the permission has been granted...
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
                     PackageManager.PERMISSION_GRANTED) {
-                // start cursor loader
+                getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
             // if permission has not been granted
             } else {
                 // determine if the application should show a custom permission request
@@ -54,8 +84,28 @@ public class EntryPointActivity extends AppCompatActivity {
                                    READ_EXTERNAL_STORAGE_PERMISSION);
             }
         } else {
-            // start cursor loader
+            getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null ,this);
         }
+    }
+
+    private Artist getArtists(int position) {
+        try {
+            int idIndex = mediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns._ID);
+            int mediaTypeIndex = mediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE);
+            mediaStoreCursor.moveToPosition(position);
+            switch (mediaStoreCursor.getInt(mediaTypeIndex)) {
+                case MediaStore.Files.FileColumns.MEDIA_TYPE_AUDIO:
+                    String artistName = MediaStore.Audio.Artists.ARTIST;
+                    Log.d("Artist Name", "ArtistName: " + artistName);
+                    String albumName = MediaStore.Audio.Albums.ALBUM;
+                    Log.d("Album Name", "AlbumName: " + albumName);
+                default:
+                    return null;
+            }
+        } catch (CursorIndexOutOfBoundsException e) {
+            Log.d("Cursor Exception", e.toString());
+        }
+        return null;
     }
 
     /**
@@ -73,15 +123,52 @@ public class EntryPointActivity extends AppCompatActivity {
             case READ_EXTERNAL_STORAGE_PERMISSION:
                 // if the permission was granted...
                 if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Thank you!", Toast.LENGTH_LONG);
                     // call cursor loader
                 // if the permission was not granted
                 } else {
                     // TODO: Warns user about permission requirements
                 }
+                break;
             // do this if no other case is accepted
             default:
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
+    private Cursor getCursor(Context context) {
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                         + MediaStore.Files.FileColumns.MEDIA_TYPE_AUDIO;
+        return context.getContentResolver().query(uri,
+                                                  projection,
+                                                  selection,
+                                                 null,
+                                                 MediaStore.Files.FileColumns.TITLE + " ASC");
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        // Set the projection statement
+
+        // Set the selection statement
+        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                         + MediaStore.Files.FileColumns.MEDIA_TYPE_AUDIO;
+        return new CursorLoader (this,
+                                Uri.parse("content://*"),
+                                projection,
+                                selection,
+                                null,
+                                MediaStore.Files.FileColumns.TITLE + " ASC");
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
 }
--- app/src/main/java/com/haydende/heymusic/MusicCollection.java
+++ app/src/main/java/com/haydende/heymusic/MusicCollection.java
@@ -1,17 +1,17 @@
 package com.haydende.heymusic;
 
+import android.database.Cursor;
 import android.provider.MediaStore;
 
 import java.util.List;
 
 public class MusicCollection {
 
+    private static Cursor mediaStoreCursor;
     private static MediaStore mediaStore = new MediaStore();
 
     public static List<Song> getSongs() {
         return null;
     }
-
-
 
 }
