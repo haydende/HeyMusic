673b1b5728209c4a2210d10c83ebdd7510993a71
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-05-17 15:36:29 +0100

    Updated AlbumGridAdapter.java
    Fixed the issue with not being able to retrieve the Album Cover. It now displays but needs to be refined to prevent overdrawing.

--- app/src/main/java/com/haydende/heymusic/AlbumGridAdapter.java
+++ app/src/main/java/com/haydende/heymusic/AlbumGridAdapter.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
 import android.text.Layout;
+import android.util.Log;
 import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -48,11 +49,11 @@ public class AlbumGridAdapter extends RecyclerView.Adapter<AlbumGridAdapter.Albu
 
     @Override
     public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
-        // Bitmap bitmap = getAlbumCover(position);
-        //if (bitmap != null) {
-            // holder.getImageButton().setImageBitmap(bitmap);
-            holder.getTextView().setText("Hello world");
-        //}
+        Bitmap bitmap = getAlbumCover(position);
+        if (bitmap != null) {
+            holder.getImageButton().setImageBitmap(bitmap);
+        }
+        holder.getTextView().setText(getAlbumName(position));
     }
 
     @Override
@@ -63,19 +64,19 @@ public class AlbumGridAdapter extends RecyclerView.Adapter<AlbumGridAdapter.Albu
     public static class AlbumViewHolder extends RecyclerView.ViewHolder {
 
         /**
-         * {@link ImageButton} view for {@code albumLayout_imageButton}
+         * {@link ImageButton} value for representing {@code albumLayout_imageButton}
          */
         private final ImageButton imageButton;
 
         /**
-         * {@link TextView} view for {@code albumLayout_text}
+         * {@link TextView} value for representing {@code albumLayout_text}
          */
         private final TextView textView;
 
         /**
          * Default constructor for this class.
          * <p>
-         *     Sets the {@code imageButton} and {@code textView} members.
+         *     Sets the {@code imageButton} and {@link #textView} members.
          * </p>
          * @param itemView The {@link View} that will be hosting the layout
          */
@@ -86,18 +87,23 @@ public class AlbumGridAdapter extends RecyclerView.Adapter<AlbumGridAdapter.Albu
         }
 
         /**
-         * Accessor for {@code imageButton}.
-         * @return {@code imageButton}
+         * Accessor for {@link #imageButton}.
+         * @return {@link #imageButton}
          */
         public ImageButton getImageButton() { return this.imageButton; }
 
         /**
-         * Accessor for {@code textView}.
-         * @return {@code textView}
+         * Accessor for {@link #textView}.
+         * @return {@link #textView}
          */
         public TextView getTextView() { return this.textView; }
     }
 
+    /**
+     * Method for providing a new {@link Cursor} value to be used by {@link #mediaStoreCursor}.
+     * @param cursor New {@link Cursor} value for {@link #mediaStoreCursor}
+     * @return Old {@link Cursor} value to be closed
+     */
     private Cursor swapCursor(Cursor cursor) {
         if (mediaStoreCursor == cursor) {
             return null;
@@ -110,6 +116,10 @@ public class AlbumGridAdapter extends RecyclerView.Adapter<AlbumGridAdapter.Albu
         return oldCursor;
     }
 
+    /**
+     * Method for finalising the change of {@link Cursor} value in {@link #mediaStoreCursor}.
+     * @param cursor Old {@link Cursor} that needs to be closed
+     */
     public void changeCursor(Cursor cursor) {
         Cursor oldCursor = swapCursor(cursor);
         if (oldCursor != null) {
@@ -117,24 +127,55 @@ public class AlbumGridAdapter extends RecyclerView.Adapter<AlbumGridAdapter.Albu
         }
     }
 
+    /**
+     * Method for getting the name for the album at the specified position in the {@link #mediaStoreCursor}.
+     * @param position position for the {@link #mediaStoreCursor} to look in
+     * @return Album name to be used for {@link this#textView}
+     */
+    private String getAlbumName(int position) {
+        mediaStoreCursor.moveToPosition(position);
+        return mediaStoreCursor.getString(
+                mediaStoreCursor.getColumnIndex(
+                        MediaStore.Audio.Albums.ALBUM
+                )
+        );
+    }
+
+    /**
+     * Method for getting the album cover as a {@link Bitmap} image.
+     * @param position Position for the mediaStoreCursor to look in
+     * @return Album cover to be used for ImageButton image
+     */
     private Bitmap getAlbumCover(int position) {
         int idIndex = mediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns._ID);
         int mediaTypeIndex = mediaStoreCursor.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE);
         try {
+            Log.d("getAlbumCover", "Starting method... ");
             mediaStoreCursor.moveToPosition(position);
-            return MediaStore.Images.Media.getBitmap(
+            Bitmap cover = MediaStore.Images.Media.getBitmap(
                     activity.getContentResolver(),
-                    Uri.parse(
+                    getAlbumUri(
                             mediaStoreCursor.getString(
                                     mediaStoreCursor.getColumnIndex(
-                                            MediaStore.Audio.Albums.ALBUM_ART
-                                    )
-                            )
-                    )
+                                        MediaStore.Audio.Albums._ID
+                            )))
             );
+            Log.d("getAlbumCover", "Returning bitmap");
+            return cover;
         } catch (IOException ioE) {
+            Log.d("getAlbumCover", "Returning null");
             return null;
         }
+    }
 
+    /**
+     * Method for getting the album cover {@link Uri} for the album retrieved from {@code mediaStoreCursor}.
+     * @param albumID Album ID value taken from mediaStoreCursor
+     * @return Album cover Uri for the image corresponding to albumID
+     */
+    private Uri getAlbumUri(String albumID) {
+        Uri artworkUri = Uri.parse("content://media/external/audio/albumart");
+        Uri imageUri = Uri.withAppendedPath(artworkUri, String.valueOf(albumID));
+        return imageUri;
     }
 }
