2826ab489d298d949e15608672dc00611c53ef24
haydende <haydeneastwell@outlook.com>
2020-05-24 15:15:38 +0100

    Added MediaPlayer functionality
    You can now play tracks in the app, though you can't stop it without waiting for it to finish or closing the app

--- app/src/main/java/com/haydende/heymusic/NowPlayingActivity.java
+++ app/src/main/java/com/haydende/heymusic/NowPlayingActivity.java
@@ -9,12 +9,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 public class NowPlayingActivity extends AppCompatActivity {
+
+    private static Uri contentUri;
 
     /**
      * {@link HashMap} for the String values for track attributes that have been passed along by the
@@ -24,15 +32,36 @@ public class NowPlayingActivity extends AppCompatActivity {
 
     private static Bitmap coverArt;
 
+    private MediaPlayer player;
+
     private RecyclerView recyclerView;
+
+    private ImageButton playPauseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
+
+        player = new MediaPlayer();
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
         recyclerView = findViewById(R.id.nowPlayingRecyclerView);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
         recyclerView.setAdapter(new NowPlayingAdapter(trackAttributes, coverArt));
+
+        playPauseButton = findViewById(R.id.nowPlayingButtons_playPause);
+        playPauseButton.setOnClickListener((View v) -> {
+            new Thread(() -> {
+                try {
+                    player.setDataSource(getApplicationContext(), contentUri);
+                    player.prepare();
+                    player.start();
+                } catch (IOException ioE) {
+
+                }
+            }).start();
+        });
     }
 
     /**
@@ -65,7 +94,16 @@ public class NowPlayingActivity extends AppCompatActivity {
         trackAttributes = newTrackAttributes;
     }
 
-    public static void setAlbumCover(Bitmap newCoverArt) {
-        coverArt = newCoverArt;
-    }
+    /**
+     * Mutator for the {@link this#coverArt}.
+     * @param newCoverArt now value for {@link this#coverArt}
+     */
+    public static void setAlbumCover(Bitmap newCoverArt) { coverArt = newCoverArt; }
+
+    /**
+     * Mutator for {@link this#contentUri}.
+     * @param newContentUri new value for {@link this#contentUri}
+     */
+    public static void setContentUri(Uri newContentUri) { contentUri = newContentUri; }
 }
+
