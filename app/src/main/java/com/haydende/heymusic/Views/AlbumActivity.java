9f3adcfddd52f5cefa92fef03545a12c0830e34d
haydende <haydeneastwell@outlook.com>
2020-06-28 11:48:44 +0100

    Added Album and Artist View Activities to the project

---
+++ app/src/main/java/com/haydende/heymusic/Views/AlbumActivity.java
@@ -0,0 +1,16 @@
+package com.haydende.heymusic.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.haydende.heymusic.R;
+
+public class AlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.album_activity);
+    }
+}
