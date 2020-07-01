5df2cad5d26cc1d1e21957e46eda191d15d2ec65
haydende <haydeneastwell@outlook.com>
2020-07-01 12:34:52 +0100

    Added reference to the ConstraintLayout used in the song_list_item and added an onClickListener to it
    The onClickListener currently only has a logging statement.

--- app/src/main/java/com/haydende/heymusic/AlbumActivity/AlbumActivityAdapter.java
+++ app/src/main/java/com/haydende/heymusic/AlbumActivity/AlbumActivityAdapter.java
@@ -2,6 +2,7 @@ package com.haydende.heymusic.AlbumActivity;
 
 import android.app.Activity;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import static android.provider.MediaStore.Audio.Media;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.haydende.heymusic.R;
@@ -36,6 +38,14 @@ public class AlbumActivityAdapter extends RecyclerView.Adapter<AlbumActivityAdap
 
     @Override
     public void onBindViewHolder(@NonNull AlbumActivityAdapter.AlbumActivityViewHolder holder, int position) {
+        holder.getWholeLayout().setOnClickListener((View v) -> {
+            Log.i("SongListItem",
+                    String.format(
+                            "OnClickListener: Track %s has been clicked",
+                            getTrackNumber(position)
+                    )
+            );
+        });
         holder.getTrackNumber().setText(getTrackNumber(position));
         holder.getTrackName().setText(getTrackName(position));
         holder.getTrackDuration().setText(getTrackDuration(position));
@@ -48,6 +58,8 @@ public class AlbumActivityAdapter extends RecyclerView.Adapter<AlbumActivityAdap
 
     public static class AlbumActivityViewHolder extends RecyclerView.ViewHolder {
 
+        private final ConstraintLayout wholeLayout;
+
         private final TextView trackNumber;
 
         private final TextView trackName;
@@ -56,10 +68,13 @@ public class AlbumActivityAdapter extends RecyclerView.Adapter<AlbumActivityAdap
 
         public AlbumActivityViewHolder(View itemView) {
             super(itemView);
+            wholeLayout = itemView.findViewById(R.id.songListItem);
             trackNumber = itemView.findViewById(R.id.songListItem_trackNum);
             trackName = itemView.findViewById(R.id.songListItem_trackName);
             trackDuration = itemView.findViewById(R.id.songListItem_trackDuration);
         }
+
+        public ConstraintLayout getWholeLayout() { return wholeLayout; }
 
         public TextView getTrackNumber() {
             return trackNumber;
